3f8b9027771bf72b9f5d4128ec604d0b0f30e451
Sean George <sean.george@excella.com>
2014-05-14 23:34:18 -0400

    added student final exercise project shell

---
+++ StudentFinalExercise/src/test/java/com/excella/cucumber/CucumberRunner.java
@@ -0,0 +1,15 @@
+package com.excella.cucumber;
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	format={"html:build/cucumber.html", "junit:build/cucumber.xml","json:build/cucumber.json"}
+		)
+public class CucumberRunner {
+
+}
